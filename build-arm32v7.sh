f33b91439d9c6fe97ccf21287c9a24f507ec377e
merlin2533 <56065775+merlin2533@users.noreply.github.com>
2019-10-02 09:38:27 +0200

    Update build-arm32v7.sh
    Update to a working image that work,

--- build-arm32v7.sh
+++ build-arm32v7.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker pull arm32v7/node:11
+docker pull arm32v6/node:12-alpine
 docker build -t virtualzone/landroid-bridge:arm32v7 -f Dockerfile.rpi .
 echo "Build finished, run the following command to push:"
 echo "docker push virtualzone/landroid-bridge:arm32v7"
