3fc816daca542b142fa0e58b5275d358f3d7af43
Heiner <heiner.peuser@outlook.com>
2020-04-05 20:11:04 +0200

    Added multi-arch Dockerfiles

--- build-arm32v7.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker pull arm32v7/node:11
-docker build -t virtualzone/landroid-bridge:arm32v7 -f Dockerfile.rpi .
-echo "Build finished, run the following command to push:"
-echo "docker push virtualzone/landroid-bridge:arm32v7"
